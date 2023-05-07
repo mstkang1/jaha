c927a6fb08491b1086b358296c60f604699a837e
mstkang1 <mstkang@gmail.com>
2023-05-08 02:01:48 +0900

    약국찾기완료1차

--- src/main/java/com/knpharm/jaha/domain/StoreVO.java
+++ src/main/java/com/knpharm/jaha/domain/StoreVO.java
@@ -1,4 +1,24 @@
 package com.knpharm.jaha.domain;
 
+import lombok.Data;
+
+
+@Data
 public class StoreVO {
+    private int storeSeq;
+
+    private String brandCode;
+
+    private String productName;
+
+    private String storeName;
+
+    private String address;
+
+    private float latitude;
+
+    private float longitude;
+
+    private int distance;
+
 }
